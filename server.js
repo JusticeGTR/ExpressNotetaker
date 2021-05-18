f7936331540c4ee9608b6dba0003cb84233efa7f
Justin Lindsey <justinlindseylhr@gmail.com>
2021-05-18 09:55:09 -0500

    updating mostly working code

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path')
 const fs = require('fs')
+const { nanoid } = require('nanoid')
+const id = nanoid();
 
 const PORT = 3000
 
@@ -10,24 +12,66 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+
 app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/db.json')));
+
+app.get('/api/notes/:id', (req, res) => {
+    const chosen = req.params.id;
+    fs.readFile('./db/db.json', 'utf8', (err, jsonString) => {
+        if (err) throw err
+        const notes = JSON.parse(jsonString);
+
+        for (let i = 0; i < notes.length; i++) {
+            if (chosen === notes[i].id) {
+                return res.json(notes[i]);
+            }
+        }
+
+        return res.json(false)
+    })
+})
+
 app.post('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, jsonString) => {
         if (err) throw err
         const notes = JSON.parse(jsonString);
+        req.body.id = id
         notes.push(req.body)
         const newNotesString = JSON.stringify(notes)
         fs.writeFile('./db/db.json', newNotesString, err => {
             if (err) {
                 console.log('Error writing file', err)
             } else {
-                res.json({ ok: true });
+                res.json    ({ ok: true });
                 console.log('Successfully wrote file')
             }
         })
     })
-    
 
+
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    const chosen = req.params.id;
+    fs.readFile('./db/db.json', 'utf8', (err, jsonString) => {
+        if (err) throw err
+        const notes = JSON.parse(jsonString);
+        // var filtered = array.filter(function(value, index, arr){ 
+        //     return value > 5;
+        // });
+        const newNotes = notes.filter((note) =>{
+            return note.id !== chosen;
+        })
+        const newNotesString = JSON.stringify(newNotes)
+        fs.writeFile('./db/db.json', newNotesString, err => {
+            if (err) {
+                console.log('Error writing file', err)
+            } else {
+                res.json    ({ ok: true });
+                console.log('Successfully wrote file')
+            }
+        })
+    })
 })
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
